50821c0582dbd0a64b7d7ec51f1ff35f983709dd
Youssef Atta'i <youssefgalalnazem@gmail.com>
2024-05-03 19:47:19 +0300

    Add todos to wrap async handlers with `asyncHandler`

--- src/features/auth/auth.routes.ts
+++ src/features/auth/auth.routes.ts
@@ -4,13 +4,19 @@ import loginHandler from "./logic/handlers/login.handler";
 import registerHandler from "./logic/handlers/register.handler";
 
 export default (router: Router) => {
+  /*
+   * Login
+   */
   router.post(
     "/login",
-    loginHandler,
+    loginHandler, // TODO: wrap in asyncHandler
   );
 
+  /*
+   * Register
+   */
   router.post(
     "/register",
-    registerHandler,
+    registerHandler, // TODO: wrap in asyncHandler
   );
 };
