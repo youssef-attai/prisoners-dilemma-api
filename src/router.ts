5301b7acfe0eae427f67af973c8e7bb18094c445
Youssef Atta'i <youssefgalalnazem@gmail.com>
2024-05-03 19:13:16 +0300

    Remove example route

--- src/router.ts
+++ src/router.ts
@@ -1,11 +1,7 @@
 import { Router } from "express";
 
-import exampleRoutes from "./features/example/example.routes";
-
 const router: Router = Router();
 
 export default (): Router => {
-  exampleRoutes(router);
-
   return router;
 };
